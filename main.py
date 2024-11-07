98c3fa6edc77e6fd65b56993148fed62e67f4608
arj5k <arjckumar55@gmail.com>
2024-11-06 22:54:12 -0500

    Chess piece class and chessboard array

--- main.py
+++ main.py
@@ -8,6 +8,51 @@ clock = pygame.time.Clock()
 
 my_rect = pygame.Rect((0, 0, 100, 100))
 
+NAMES = {
+    1:"pawn",
+    2:"knight",
+    3:"bishop",
+    4:"rook",
+    5:"queen",
+    6:"king"
+}
+VALUES = {
+    1: 1,
+    2: 3,
+    3: 3,
+    4: 5,
+    5: 9,
+    6: 100000
+    }
+class Piece:
+    def __init__(self, name, color):
+        self.name = NAMES[name]
+        self.color = color
+        self.value = VALUES[name]
+WIDTH = 8
+chessboard = [["" for x in range(WIDTH)] for y in range(WIDTH)]
+
+for z in range(0,WIDTH):
+    chessboard[1][z] = Piece(1, "black")
+    chessboard[6][z] = Piece(1,"white")
+chessboard[0][0] = Piece(4, "black")
+chessboard[7][0] = Piece(4, "white")
+chessboard[0][1] = Piece(2,"black")
+chessboard[7][1] = Piece(2,"white")
+chessboard[0][2] = Piece(3, "black")
+chessboard[7][2] = Piece(3, "white")
+chessboard[0][3] = Piece(5,"black")
+chessboard[7][3] = Piece(5,"white")
+chessboard[0][4] = Piece(6,"black")
+chessboard[7][4] = Piece(6,"white")
+chessboard[0][5] = Piece(3, "black")
+chessboard[7][5] = Piece(3,"white")
+chessboard[0][6] = Piece(2,"black")
+chessboard[7][6] = Piece(2,"white")
+chessboard[0][7] = Piece(4, "black")
+chessboard[7][7] = Piece(4, "black")
+
+
 while True:
     # Process player inputs.
     for event in pygame.event.get():
