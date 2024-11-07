c442b9c080e0931bcfd5ce824b5c12d9ea3d12f6
arj5k <arjckumar55@gmail.com>
2024-11-07 00:09:41 -0500

    Chess piece class, chessboard array, and graphics rendered.  The starting setup is coded in

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((690,690))
+screen = pygame.display.set_mode((750,750))
 
 clock = pygame.time.Clock()
 
@@ -13,7 +13,7 @@ background_color = (48,46,43)
 
 NAMES = {
     1:"pawn",
-    2:"knight",
+    2:"night", #should be knight - edited so that the name starts with 'n'
     3:"bishop",
     4:"rook",
     5:"queen",
@@ -32,28 +32,52 @@ class Piece:
         self.name = NAMES[name]
         self.color = color
         self.value = VALUES[name]
+    def get_name(self):
+        return self.name
+    def get_color(self):
+        return self.color
 WIDTH = 8
-chessboard = [[0 for x in range(WIDTH)] for y in range(WIDTH)]
+chessboard = [[None for x in range(WIDTH)] for y in range(WIDTH)]
 
 for z in range(WIDTH):
     chessboard[1][z] = Piece(1, "black")
     chessboard[6][z] = Piece(1,"white")
+# Set up chessboard (8x8 grid)
+WIDTH = 8
+chessboard = [[None for x in range(WIDTH)] for y in range(WIDTH)]
+
+# Place pawns
+for z in range(WIDTH):
+    chessboard[1][z] = Piece(1, "black")
+    chessboard[6][z] = Piece(1, "white")
+
+# Place rooks
 chessboard[0][0] = Piece(4, "black")
 chessboard[7][0] = Piece(4, "white")
-chessboard[0][1] = Piece(2,"black")
-chessboard[7][1] = Piece(2,"white")
+chessboard[0][7] = Piece(4, "black")
+chessboard[7][7] = Piece(4, "white")
+
+# Place knights
+chessboard[0][1] = Piece(2, "black")
+chessboard[7][1] = Piece(2, "white")
+chessboard[0][6] = Piece(2, "black")
+chessboard[7][6] = Piece(2, "white")
+
+
+# Place bishops
 chessboard[0][2] = Piece(3, "black")
 chessboard[7][2] = Piece(3, "white")
-chessboard[0][3] = Piece(5,"black")
-chessboard[7][3] = Piece(5,"white")
-chessboard[0][4] = Piece(6,"black")
-chessboard[7][4] = Piece(6,"white")
 chessboard[0][5] = Piece(3, "black")
-chessboard[7][5] = Piece(3,"white")
-chessboard[0][6] = Piece(2,"black")
-chessboard[7][6] = Piece(2,"white")
-chessboard[0][7] = Piece(4, "black")
-chessboard[7][7] = Piece(4, "black")
+chessboard[7][5] = Piece(3, "white")
+
+# Place queens
+chessboard[0][3] = Piece(5, "black")
+chessboard[7][3] = Piece(5, "white")
+
+# Place kings
+chessboard[0][4] = Piece(6, "black")
+chessboard[7][4] = Piece(6, "white")
+
 
 
 while True:
@@ -68,10 +92,27 @@ while True:
 
     # Do logical updates here.
     screen.fill(background_color)  # Fill the display with a solid color
-    screen.blit(background_image, (20, 20))
+    screen.blit(background_image, (50, 50))
 
     # Render the graphics here.
+    for i in range(WIDTH):
+        for j in range(WIDTH):
+            if chessboard[i][j]:
+                piece = chessboard[i][j]
+                piece_color = piece.get_color()
+                piece_name = piece.get_name()
 
+                # Determine the image based on piece color and name
+                piece_key = f"{piece_color[0]}{piece_name[0]}.png"  # e.g., 'wp.png'
+                img = pygame.image.load(piece_key)
+                img = pygame.transform.scale(img, (background_image.get_width() //8 , background_image.get_height()//8))
+
+                # Calculate position on the screen
+                x_val = 50 + j * (background_image.get_width() // WIDTH)
+                y_val = 50 + i * (background_image.get_height() // WIDTH)
+
+                # Draw the piece image on the board
+                screen.blit(img, (x_val, y_val))
 
     pygame.display.flip()  # Refresh on-screen display
     clock.tick(60)
